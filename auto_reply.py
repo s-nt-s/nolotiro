c04adf15ba117c9dd73b37e8846c513dad45ea9e
s-nt-s <santos82h@gmail.com>
2019-11-07 08:53:47 +0100

    diferenciar entre articulos pesados o livianos

--- auto_reply.py
+++ auto_reply.py
@@ -5,7 +5,10 @@ from os import chdir
 from os.path import abspath, dirname, isfile
 
 from core.api import NoLoTiro
-from core.util import cfg, read, read_tuples
+from core.util import cfg, read, read_tuples, breakline
+
+import re
+import sys
 
 abspath = abspath(__file__)
 dname = dirname(abspath)
@@ -15,18 +18,25 @@ msg_template = read("msg.txt")
 user, password = cfg(".pw_nolotiro")
 
 done_file = "done.txt"
+heavy_file = "heavy.txt"
 
 done = set(read_tuples(done_file)) if isfile(done_file) else set()
+heavy = set(read(heavy_file).split("\n")) if isfile(heavy_file) else set()
 
 
 def reply(t):
-    t.reply(msg_template % (t.sender))
+    mark = "//" if t and t.subject in heavy else "%%"
+    msg = re.sub(r"^---.*$", "", msg_template, flags=re.MULTILINE)
+    msg = re.sub(r"^"+mark+r" .*$", "", msg, flags=re.MULTILINE)
+    msg = re.sub(r"^(%%|//) ", "", msg, flags=re.MULTILINE)
+    msg = breakline.sub(r"\n\n", msg)
+    t.reply(msg % (t.sender))
 
 n = NoLoTiro(user, password)
 
 for t in n.threads():
     if t.key not in done:
-        if t.unread and not t.answered:
+        if not t.answered:
             reply(t)
         done.add(t.key)
 
